32abbcda6a191d47f52123e3fb0f5bd644346fae
MichaelGeel <mnrmichaelgeel@gmail.com>
2024-03-28 14:34:58 +0200

    Post lesson 4 commit.
    Have covered lesson 3 and 4, which re-iterated the installation and hello world along with a tutorial in the use of pipenv which combines the functionalities of pip and a virtual environment.

--- app.py
+++ app.py
@@ -8,13 +8,14 @@ app = Flask(__name__)
 
 # Creating a route using the Flask object.
 # <name> is not html, it's a placeholder.
-@app.route('/<name>')
+#Will remove the name placeholder for now.
+@app.route('/')# <name>')
 # Defining the function tied to the route.
 # Added the name parameter to match the <name> placeholder.
-def index(name):
+def index(): # name):
         # For now we'll just return the below HTML code.
         # Have now added a name variable using the placeholer and format.
-        return '<h1>Hello {}!</h1>'.format(name)
+        return '<h1>Hello, World!</h1>' # {}!</h1>'.format(name)
 
-if __name__ == 'main':
+if __name__ == '__main__':
         app.run()
